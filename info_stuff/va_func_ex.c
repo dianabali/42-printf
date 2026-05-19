1ffdc46eae62bffbcd7df0e9dc2cfee9e20fcac3
Diana Bali <dianabali64@gmail.com>
2026-05-19 14:58:39 +0200

    va_func_ex

--- info_stuff/va_func_ex.c
+++ info_stuff/va_func_ex.c
@@ -4,15 +4,26 @@
 int	sum(int count, ...)
 {
 	va_list args; // va_list to keep track where we are while reading args (iterator)
+	va_list copy;
 
 	va_start(args, count); // "The extra args begin after count"
+	va_copy(copy, args); // Copies args
 
-	int total = 0;
+	int total_1 = 0;
+	int total_2 = 0;
 
+	// First traversal using args
 	for (int i = 0; i < count; i++)
-		total = total + va_arg(args, int); // Get the next arg and add
+		total_1 = total_1 + va_arg(args, int); // Get the next arg and add
+
+	// Second traversal using copy
+	for (int i = 0; i < count; i++)
+        total_2 = total_2 + va_arg(copy, int);
+	
 	va_end(args); // Finishes the arg reading
-	return (total);
+	va_end(copy);
+
+	return (total_1);
 }
 
 int	main(void)
