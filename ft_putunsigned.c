931b186faf21893f97512d1c917fdb0797f6b5e1
Diana Bali <dianabali64@gmail.com>
2026-05-20 22:07:33 +0200

    ft_putunsigned

--- ft_putunsigned.c
+++ ft_putunsigned.c
@@ -10,21 +10,28 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+	ft_putunsigned
+	Prints an unsigned integer.
+	Parameters:
+		- n: The unsigned int to print.
+*/
+
 #include "ft_printf.h"
-// #include <stdio.h>
+#include <stdio.h>
 
 int	ft_putunsigned(unsigned int n)
 {
-	int	count;
+	int	count; // The total number of characters printed
 
 	count = 0;
-	if (n >= 10)
+	if (n >= 10) // If n has more than 1 digit
 		count += ft_putunsigned(n / 10);
-	count += ft_putchar((char)(n % 10 + '0'));
+	count += ft_putchar((char)(n % 10 + '0')); // Convert to ASCII and print
 	return (count);
 }
 
-/* int	main(void)
+int	main(void)
 {
 	printf("%u", ft_putunsigned(-5));
-} */
+}
