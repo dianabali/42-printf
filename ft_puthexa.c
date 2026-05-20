abe30c7c2583812712e5d83abb63d2e1640bb9bb
Diana Bali <dianabali64@gmail.com>
2026-05-20 21:56:28 +0200

    ft_puthexa

--- ft_puthexa.c
+++ ft_puthexa.c
@@ -10,31 +10,40 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+	ft_puthexa
+	Prints a number in hexadecimal.
+	Parameters:
+		- n: The number to convert.
+		- uppercase: If non-zero, uses uppercase hex digits (A-F), otherwise lowercase (a-f).
+*/
+
 #include "ft_printf.h"
 #include <stdio.h>
 
 int	ft_puthexa(unsigned long n, int uppercase)
 {
-	char	*base_lower;
-	char	*base_upper;
-	char	*base;
-	int		count;
+	char	*base_lower; // Lowercase digits
+	char	*base_upper; // Uppercase digits
+	char	*base; // Which base will be used
+	int		count; // Total number of characters printed
 
 	base_lower = "0123456789abcdef";
 	base_upper = "0123456789ABCDEF";
-	if (uppercase)
+	if (uppercase) // If uppercase flag is set (1 or 0)
 		base = base_upper;
 	else
 		base = base_lower;
 	count = 0;
-	if (n >= 16)
+	if (n >= 16) // If n has more than one hex digit
 		count += ft_puthexa(n / 16, uppercase);
-	count += ft_putchar(base[n % 16]);
+	count += ft_putchar(base[n % 16]); // Print the digit
 	return (count);
 }
 
 int	main(void)
 {
-	printf("%x\n", 10);
-	printf("%X\n", 10);
+	ft_puthexa(10, 0);
+	ft_putchar('\n');
+	ft_puthexa(10, 1);
 }
