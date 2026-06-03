fe9a0f8ef969aa8c7b565c04a779f7b5255e229a
Diana <dianabali64@gmail.com>
2026-06-03 18:35:45 +0200

    flags_ex.c

--- info_stuff/flags_ex.c
+++ info_stuff/flags_ex.c
@@ -20,31 +20,31 @@ void printf_flag_ex() {
     // 4. Always show sign (+)
     printf("4. Show sign      : |%+d| |%+d|\n", num, neg);
 
-	  // 5. Space before positive number ( )
+	// 5. Space before positive number ( )
     printf("5. Space flag     : |% d| |% d|\n", num, neg);
 
     // 6. Alternate form (#) - hexadecimal
     printf("6. Hex alternate  : |%#x|\n", hex);
 
-	  // 7. Alternate form (#) - octal
+	// 7. Alternate form (#) - octal
     printf("7. Octal alternate: |%#o|\n", hex);
 
     // 8. Precision for floating point
     printf("8. Precision      : |%.2f|\n", pi);
 
     // 9. Width + precision
-	  // Width = 10, Precision = 2
+	// Width = 10, Precision = 2
     printf("9. Width+precision: |%10.2f|\n", pi);
 
     // 10. String precision (max chars printed)
-	  // Prints only 5 characters
+	// Prints only 5 characters
     printf("10. String cut     : |%.5s|\n", "HelloWorld");
 }
 
 int main() {
     printf_flag_ex();
 
-	// Left align, Always hsow sign, Width 10
+	// Left align, Always show sign, Width 10
 	printf("\nCombination of flags: %-+10d", 42);
     return 0;
 }
